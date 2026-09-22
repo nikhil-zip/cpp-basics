d0c29e0494d39d98e9a5bb0d0a8f12acd5b57b4f
nikhil-mca-code <nikhil.mca.in@gmail.com>
2026-09-22 18:09:33 +0530

    new-bmi.c

---
+++ c-lang/practice/bmi.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+   int main() {
+       float weight, height, bmi;
+       printf("Enter your weight in kg: ");
+       scanf("%f", &weight);
+       printf("Enter your height in meters: ");
+       scanf("%f", &height);
+       
+       bmi = weight / (height * height);
+       
+       printf("Your BMI is: %.2f\n", bmi);
+       
+       if (bmi < 18.5) {
+           printf("You are underweight.\n");
+       } else if (bmi >= 18.5 && bmi < 24.9) {
+           printf("You have a normal weight.\n");
+       } else if (bmi >= 25 && bmi < 29.9) {
+           printf("You are overweight.\n");
+       } 
+       
+       return 0;
+   }
